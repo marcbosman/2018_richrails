41172910e207add2d7062ff05a3c2b1a2f4ceeef
marcbosman <marc.bosman@live.nl>
2018-12-03 11:46:02 +0100

    Basis van klasse Train

--- richrails/src/Train/Train.java
+++ richrails/src/Train/Train.java
@@ -1,5 +1,27 @@
 package Train;
 
-public class Train {
+import java.util.ArrayList;
 
+public class Train {
+	private ArrayList<Component> allComponents;
+	private String name;
+	private int trainID;
+	
+	public Train(String name) {
+		this.name = name;
+	}
+	
+	public void addComponent(Component c) {
+		//todo
+	}
+	
+	public int calculateSeats() {
+		//todo
+		return 0;
+	}
+	
+	public String toString() {
+		//todo
+		return "";
+	}
 }
