4c6d88010a1796b42fc9e7ce34bd4f56daf7701b
Thim <Thim@Tie-fighter.wifi.hu.nl>
2018-12-03 11:57:24 +0100

    Basis voor Wagon en Component

--- richrails/src/Train/Wagon.java
+++ richrails/src/Train/Wagon.java
@@ -1,5 +1,12 @@
 package Train;
 
-public class Wagon {
-
+public class Wagon implements Component {
+	private String name;
+	private int wagonID;
+	private int seats;
+	
+	@Override
+	public Component createComponent() {
+		//todo
+	}
 }
