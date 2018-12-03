55e12ffc52c613624dd1977969edb3c1808c9539
rubenshu <ruben.sikkens@student.hu.nl>
2018-12-03 11:55:22 +0100

    ComponentFactory updated. Commented out because super/sub structure is not yet created.

--- richrails/src/Train/ComponentFactory.java
+++ richrails/src/Train/ComponentFactory.java
@@ -1,5 +1,19 @@
 package Train;
 
 public class ComponentFactory {
-
-}
+	
+	   //use getShape method to get object of type shape 
+	   public Component getComponent(String componentType){
+	      if(componentType == null){
+	         return null;
+	      }		
+//	      if(componentType.equalsIgnoreCase("LOCOMOTIVE")){
+//	         return new Locomotive();
+//	         
+//	      } else if(componentType.equalsIgnoreCase("WAGON")){
+//	         return new Wagon();
+//	      }
+	      
+	      return null;
+	   }
+	}
