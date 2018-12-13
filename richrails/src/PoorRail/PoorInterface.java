eae9c3dc909936bb28bb714e90e572436e2d6862
Thim <Thim@Tie-fighter.wifi.hu.nl>
2018-12-13 10:52:17 +0100

    Geen idee

--- richrails/src/PoorRail/PoorInterface.java
+++ richrails/src/PoorRail/PoorInterface.java
@@ -345,23 +345,23 @@ public class PoorInterface extends javax.swing.JFrame implements ActionListener
 		}
 	}
 		
-		public void drawLocomotive(String train) 
-		{
-			if (train != "")
-			{
-				Graphics g = drawPanel.getGraphics();
-				g.setColor(Color.LIGHT_GRAY);
-				g.fillRect(30,80+currentTrain*OFFSET,80,40);
-				g.fillRect(80,60+currentTrain*OFFSET,30,30);
-				g.drawRoundRect(85, 40+currentTrain*OFFSET, 20, 20, 20, 20);
-				g.drawRoundRect(85, currentTrain*OFFSET, 40, 40, 40, 40);
-				g.setColor(Color.BLACK);
-				g.fillRoundRect(35, 120+currentTrain*OFFSET, 20, 20, 20, 20);
-				g.fillRoundRect(80, 120+currentTrain*OFFSET, 20, 20, 20, 20);
-				g.drawString(train,40,105+currentTrain*OFFSET);
-			}
-	    }
-	
+//		public void drawLocomotive(String train) 
+//		{
+//			if (train != "")
+//			{
+//				Graphics g = drawPanel.getGraphics();
+//				g.setColor(Color.LIGHT_GRAY);
+//				g.fillRect(30,80+currentTrain*OFFSET,80,40);
+//				g.fillRect(80,60+currentTrain*OFFSET,30,30);
+//				g.drawRoundRect(85, 40+currentTrain*OFFSET, 20, 20, 20, 20);
+//				g.drawRoundRect(85, currentTrain*OFFSET, 40, 40, 40, 40);
+//				g.setColor(Color.BLACK);
+//				g.fillRoundRect(35, 120+currentTrain*OFFSET, 20, 20, 20, 20);
+//				g.fillRoundRect(80, 120+currentTrain*OFFSET, 20, 20, 20, 20);
+//				g.drawString(train,40,105+currentTrain*OFFSET);
+//			}
+//	    }
+//	
 	
 	// FUNCTIES GEBRUIKT IN DE GUI(buttons)
 //	public String addTrain(String train)
